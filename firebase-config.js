43c601aada56196eb95f3ee5130c97cbf5a34823
siam6988 <fakegmlsr@gmail.com>
2025-11-12 19:02:19 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+
+// Your web app's Firebase configuration
+// ভবিষ্যতে Firebase সংযোগ করার জন্য এই ফাইলটি ব্যবহার করা হবে।
+const firebaseConfig = {
+  apiKey: "AIzaSyDSYALT_jaIQrTq-oZP9sMyUJWXaLSjTY4",
+  authDomain: "nur-shop-siam.firebaseapp.com",
+  projectId: "nur-shop-siam",
+  storageBucket: "nur-shop-siam.firebasestorage.app",
+  messagingSenderId: "536596371721",
+  appId: "1:536596371721:web:454f879521237e61211bd8"
+};
+
+// Initialize Firebase (এই লাইনটি কমেন্ট আউট থাকবে যতক্ষণ না আপনি Firebase SDK যুক্ত করছেন)
+// firebase.initializeApp(firebaseConfig);
